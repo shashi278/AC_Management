110f4c0186603ccc49493693f8dac1016d863e42
Shashi Ranjan <shashiranjankv@gmail.com>
2019-05-20 10:29:14 +0530

    Add files via upload

---
+++ project/database.py
@@ -0,0 +1,170 @@
+
+import sqlite3
+from sqlite3 import Error
+import xlrd
+import time
+from time import strftime
+import os
+
+tm= time.localtime()
+tmStmp=  "{}{}{}".format(tm.tm_mday,tm.tm_mon,tm.tm_year)
+
+class Database():
+
+	if not os.path.exists("database/"):
+		os.makedirs("database/")
+
+	# .db file
+	db= ""
+
+	#Insert into database
+	def insert_into_database(self, tableName , conn, data):
+		if conn is not None:
+			try:
+				c= conn.execute("select * from {}".format(tableName))
+				fields= tuple([des[0] for des in c.description][1:])
+				print(fields)
+				print(data)
+				cur= conn.cursor()
+				cur.execute(
+					"""
+					INSERT INTO {}
+					{} VALUES {}
+					""".format(tableName, fields, data)
+					)
+				conn.commit()
+			except Error as e:
+				print("Error in data insertion: {}".format(e))
+
+
+	#Update Database
+	def update_database(self, tableName, conn, index, field, field_val):
+		if conn is not None:
+			try:
+				cur= conn.cursor()
+				cur.execute(
+						"""
+						UPDATE {}
+						SET {}= ? WHERE id= ?
+						""".format(tableName, field),
+						(field_val, index)
+					)
+				conn.commit()
+
+			except:
+				print("Error in updating data: {}".format(e))
+
+
+	#Delete from Database
+	def delete_from_database(self,tableName, conn, index):
+		if conn is not None:
+			try:
+				cur= conn.cursor()
+				cur.execute(
+						"""
+						DELETE FROM {} WHERE id= ?
+						""".format(tableName), (index,)
+					)
+				conn.commit()
+			except Error as e:
+				print("Error in deleting data: {}".format(e))
+
+
+	#Search in the database
+	def search_from_database(self,tableName, conn, prop, value):
+		if conn is not None:
+			try:
+				cur= conn.cursor()
+				filtered_list= cur.execute(
+						"""
+							SELECT * FROM {} WHERE {} LIKE ? ORDER BY id;
+						""".format(tableName, prop),
+						(value+'%',)
+					).fetchall()
+
+				return filtered_list
+			except Error as e:
+				print("Error in searching: {}".format(e))
+
+			return None
+
+
+	#connect database
+	def connect_database(self):
+		try:
+			conn= sqlite3.connect(self.db)
+			return conn
+
+		except Error as e:
+			print("Error in databse: {}".format(e))
+
+		return None
+
+	#create table
+	def create_table(self, table, conn):
+
+		if conn is not None:
+			try:
+				cur= conn.cursor()
+				cur.execute(table)
+				conn.commit()
+
+			except Error as e:
+				print("Error in creating table: {}".format(e))
+
+		else: print("conn is none")
+
+	def findTables(self, db_file):
+		self.db= "database/"+db_file
+		conn= self.connect_database()
+		if conn is not None:
+			cur= conn.cursor()
+			cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+			return [each[0] for each in cur.fetchall()]
+		conn.close()
+
+		
+	def readFile(self, db_file, table, tableName, file_name ):
+
+		table= table.format(tableName)
+
+		wb= xlrd.open_workbook(file_name)
+		sheet= wb.sheet_by_index(0)
+		self.db= "database/"+db_file
+		conn= self.connect_database()
+
+		if conn is not None:
+			self.create_table(table,conn)
+
+			for i in range(1,sheet.nrows):
+				data= (sheet.cell_value(i,0),sheet.cell_value(i,1),sheet.cell_value(i,2))
+				self.insert_into_database(tableName,conn, data)
+
+	def addData(self, db_file, table, tableName, data):
+
+		table= table.format(tableName)
+		self.db= self.db= "database/"+db_file
+		conn= self.connect_database()
+
+		if conn is not None:
+			self.create_table(table, conn)
+			self.insert_into_database(tableName, conn, data)
+
+	def extractAllData(self, db_file, tableName):
+
+		self.db= "database/"+db_file
+		conn= self.connect_database()
+
+		if conn is not None:
+			cur= conn.execute("SELECT * FROM {}".format(tableName))
+			return cur.fetchall()
+
+
+
+if __name__ == '__main__':
+
+	db= Database()
+	#db.readFile("C:\\Users\\Anand Kumar\\Desktop\\project\\student.xlsx", "year_2017_2021")
+	conn= db.connect_database()
+	#db.delete_from_database("2017_2021",conn, 24)
+	print(db.search_from_database("year_2022_2019",conn, 'name', 'ajit'))
---
+++ project/main.py
@@ -0,0 +1,430 @@
+
+"""
+	
+	Account Management System
+	Date: 24/02/2019
+
+"""
+
+#imports here
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.modalview import ModalView
+from kivy.uix.popup import Popup
+from kivy.uix.button import Button
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.textinput import TextInput
+from kivy.uix.behaviors.touchripple import TouchRippleBehavior
+from kivy.uix.dropdown import DropDown
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty, NumericProperty
+from kivy.core.window import Window, WindowBase
+from kivy.animation import Animation
+from kivymd.theming import ThemeManager
+from kivymd.button import MDRaisedButton
+from kivymd.textfields import MDTextField
+import xlrd
+
+import os
+from os.path import sep, expanduser, isdir, dirname
+from kivy.garden.filebrowser import FileBrowser
+from kivy.utils import platform
+
+from database import Database
+import re
+
+usernameHash= ''
+passwordHash= ''
+
+###########################Global variables##########################
+yearNameList=["Ist Year","2nd Year","3rd Year","4th Year"]
+txt=[]
+#####################################################################
+class InfoPopup(ModalView):
+	pass
+class UserInfoEdit(ModalView):
+	pass
+
+class UpdateStudentInfo(ModalView):
+	pass
+
+class DeleteWarnPopup(ModalView):
+	def onHitConfirm(self):
+		#data base part
+		#self.ids.label.text="Are you sure to delete year "++" and batch "+
+		pass
+class CircularToggleButton(BoxLayout,ToggleButton):
+	pass
+
+class DropBtn(MDRaisedButton):
+	def select(self, instance):
+		pass
+
+
+#dropDown class for Session
+class SessionDrop(DropDown):
+
+	def on_open(self):
+		pass
+
+	def on_dismiss(self):
+		pass
+
+#dropDown class for Course&Stream
+class CourseDrop(DropDown):
+	pass
+
+#dropDown class for year
+class YearDrop(DropDown):
+	pass
+
+#dropDown class for Semester
+class SemesterDrop(DropDown):
+	
+	def on_open(self):
+		pass
+
+	def on_dismiss(self):
+		pass
+
+
+#sidenav class
+class SideNav(ModalView,Database):
+	#SessionBtn=ObjectProperty()
+	#CourseBtn=ObjectProperty()
+	def openSessionList(self,instance):
+		dropdown= SessionDrop()
+		dropdown.open(instance)
+		dropdown.bind(on_select=lambda instance, btn: self.onSelect(instance, btn,self.ids.sessionBtn))
+
+	def openCourseList(self,instance):
+		dropdown= CourseDrop()
+		dropdown.open(instance)
+		dropdown.bind(on_select=lambda instance, btn: self.onSelect(instance,btn, self.ids.courseBtn))
+
+	def openYearList(self,instance):
+		dropdown= YearDrop()
+		
+		if(self.ids.sessionBtn.text=="Current Session"):
+			for name in yearNameList:			#yearNameList global variable
+				dropBtn= DropBtn(text=name)
+				dropBtn.bind(on_release=lambda dropBtn: dropdown.select(dropBtn))
+				dropdown.add_widget(dropBtn)
+
+		if(self.ids.sessionBtn.text=="All"):
+			table_list= self.findTables("student_main.db")
+			for name in table_list:
+				years= re.search(".*_(\\d+)_(\\d+)", name)
+				txt1=("{}-{}".format(years.group(1), years.group(2)))
+				dropBtn= DropBtn(text=txt1)
+				dropBtn.bind(on_release=lambda dropBtn: dropdown.select(dropBtn))
+				dropdown.add_widget(dropBtn)
+
+
+		dropdown.open(instance)
+		dropdown.bind(on_select=lambda instance, btn: self.onSelect(instance,btn, self.ids.yearBtn))
+		
+
+	def onSelect(self, inst, btn, mainBtn):
+		mainBtn.text= btn.text
+		print("Button text=",btn.text)
+		if(btn.text=="Current Session"):
+			self.ids.yearBtn.text=yearNameList[0]
+		if(btn.text=="All"):
+			self.ids.yearBtn.text=txt[0]       #txt is global variable(delclared at on_start) for instant action of year btn
+
+
+#popups class
+class LoginPopup(ModalView, Database):
+	#loginTitle= ObjectProperty()
+
+	def login(self, username, password):
+		self.username= username
+		self.password= password
+
+		#print("username: ", self.username)
+		#print("password: ", self.password)
+
+		if(self.username==usernameHash and self.password==passwordHash):
+			self.ids.warningInfo.text=""
+			self.dismiss()
+			return True
+
+		else:
+			self.ids.warningInfo.text="Wrong username or password"
+			return False
+
+
+#ScreenManager Class
+class ScreenManager(ScreenManager):
+	pass
+
+
+#HomeScreen Class
+class HomeScreen(Screen):
+	
+
+	#function to open login popup
+	def openLoginPop(self, title):
+		lp= LoginPopup()
+		lp.ids.loginTitle.text= "{} Login".format(title)
+		lp.open()
+
+
+
+#UserScreen
+class UserScreen(Screen,Database):
+
+	def openSemesterList(self,instance):
+		semesterBtn=self.ids.semesterBtn
+		dropdown= SemesterDrop()
+		dropdown.open(instance)
+		dropdown.bind(on_select=lambda instance, btn: self.onSelect(btn, self.ids.semesterBtn))
+
+	def onSelect(self, btn, mainBtn):
+		mainBtn.text= btn.text
+		print(btn.id)
+
+	
+	def anim_in(self, instance):
+		
+		anim= Animation(
+				pos_hint={'x':-.3},
+				t='in_cubic',
+				d=0.3
+			)
+		anim.start(instance)
+
+	def anim_out(self, instance):
+		anim= Animation(
+				pos_hint={'x':0},
+				t='out_cubic',
+				d=0.3
+			)
+		anim.start(instance)
+
+	def open_sideNav(self):
+		sn= SideNav()
+		sn.open()
+
+
+
+#AdminScreen
+class AdminScreen(Screen, Database):
+	pc_userName= os.getlogin()
+
+	def onStartAdminScr(self):
+		#--------------Update User info Data List ---------------------#
+		self.ids.rv.data=[]
+		data_list= self.extractAllData("user_all.db", "users")
+		for each in data_list:
+			x={
+			"name": each[1],
+			"email": each[2],
+			"username": each[3],
+			"password": each[4]
+			}
+			self.ids.rv.data.insert(0,x)
+
+		#print(self.ids.rv.data)
+		#--------------------------------------------#
+
+	def change_screen(self, instance):
+		if instance.text == 'Manage User':
+			self.ids.scrManager.current = 'manageUser'
+		elif instance.text == 'Manage Student':
+			self.ids.scrManager.current = 'manageStudent'
+		else:
+			self.ids.scrManager.current = 'adminSetting'
+
+	def Add_User_Layout(self):
+		target=self.ids.dyn_input
+		usr_name=TextInput(size_hint=(.2,1),hint_text="Name",write_tab=False)
+		usr_email=TextInput(size_hint=(.2,1),hint_text="E-mail id",write_tab=False)
+		usr_username=TextInput(size_hint=(.2,1),hint_text="Username",write_tab=False)
+		usr_password=TextInput(size_hint=(.2,1),hint_text="Password")
+		usr_submit=MDRaisedButton(size_hint=(.2,1),text="Submit",on_release=lambda x: self.Add_User(usr_name.text,usr_email.text,usr_username.text,usr_password.text))
+
+		target.add_widget(usr_name)
+		target.add_widget(usr_email)
+		target.add_widget(usr_username)
+		target.add_widget(usr_password)
+		target.add_widget(usr_submit)
+
+	def Add_User(self,name,email,username,password):
+		print(name+" "+email+" "+username+" "+password)
+
+		if(all([not len(each) for each in [name, email,username, password]])):
+			print("here")
+			return
+
+		self.ids.rv.data.insert(0, {'name': name,'email':email ,'username':username,'password':password })
+		self.ids.addusrBtn.state="normal"
+		layout=self.ids.dyn_input
+		layout.clear_widgets()
+
+		table=	"""
+					CREATE TABLE IF NOT EXISTS {} (
+					id INTEGER PRIMARY KEY,
+					name VARCHAR NOT NULL,
+					email VARCHAR NOT NULL,
+					username VARCHAR NOT NULL,
+					pass VARCHAR NOT NULL
+					)
+				"""
+		data= (name, email, username, password)
+		self.addData("user_all.db", table, "users", data)
+	def connectFileSelector(self, fromYear, toYear,btntxt):
+		if(fromYear=="" or toYear=="" ):
+			op=InfoPopup()
+			op.ids.label.text="Please fill All required Fields !"
+			op.open()
+		elif(btntxt=="Select Course"):
+			op=InfoPopup()
+			op.ids.label.text="Please Select Course !"
+			op.open()
+		else:
+			self.open_FileSelector(fromYear,toYear)
+
+	def open_FileSelector(self, fromYear, toYear):
+
+		#global fromYear
+		#fromYear= fromYear
+
+		self.table_name= "year_{}_{}".format(fromYear,toYear)
+
+		if platform == 'win':
+			user_path = dirname(expanduser('~')) + sep + 'Documents'
+		else:
+			user_path = expanduser('~') + sep + 'Documents'
+		self._fbrowser = FileBrowser(select_string='Select',favorites=[(user_path, 'Documents')], filters=["*.xls", "*.xlsx"])
+		self._fbrowser.bind(on_success=self._fbrowser_success,
+							on_canceled=self._fbrowser_canceled)
+		global fpopup
+		fpopup = Popup(content=self._fbrowser,title_align="center",title="Select File",
+						size_hint=(0.7, 0.9), auto_dismiss=True)
+		fpopup.open()
+
+	def _fbrowser_canceled(self, instance):
+		print ('cancelled, Close self.')
+		fpopup.dismiss()
+
+
+	def _fbrowser_success(self, instance):
+		 selected_path= instance.selection[0]
+		 fpopup.dismiss()
+
+		 table=	"""
+					CREATE TABLE IF NOT EXISTS {} (
+					id INTEGER PRIMARY KEY,
+					roll_no VARCHAR NOT NULL,
+					reg_no INT NOT NULL,
+					name VARCHAR NOT NULL
+					)
+				"""
+
+		 self.readFile("student_main.db", table, self.table_name, selected_path)
+		 
+
+	def openEditPopup(self):					#for edit user information open popup
+		UserInfoEdit().open()
+
+	#-----------------------select Cousrse---------------------------------------------#
+	def openCourseList(self,instance):
+		dropdown= CourseDrop()
+		dropdown.open(instance)
+		dropdown.bind(on_select=lambda instance, btn: self.onSelect(instance,btn, (self.ids.courseBtn1)))
+	def openCourseList1(self,instance):
+		dropdown= CourseDrop()
+		dropdown.open(instance)
+		dropdown.bind(on_select=lambda instance, btn: self.onSelect(instance,btn, self.ids.courseBtn2))
+	def openCourseList2(self,instance):
+		dropdown= CourseDrop()
+		dropdown.open(instance)
+		dropdown.bind(on_select=lambda instance, btn: self.onSelect(instance,btn, self.ids.courseBtn3))
+
+	def openYearList(self,instance):
+		dropdown= YearDrop()
+		table_list= self.findTables("student_main.db")
+		for name in table_list:
+			years= re.search(".*_(\\d+)_(\\d+)", name)
+			txt1=("{}-{}".format(years.group(1), years.group(2)))
+			dropBtn= DropBtn(text=txt1)
+			dropBtn.bind(on_release=lambda dropBtn: dropdown.select(dropBtn))
+			dropdown.add_widget(dropBtn)
+
+		dropdown.open(instance)
+		dropdown.bind(on_select=lambda instance, btn: self.onSelect(instance,btn, self.ids.yearBtn))
+
+	def openYearList1(self,instance):
+		dropdown= YearDrop()
+		table_list= self.findTables("student_main.db")
+		for name in table_list:
+			years= re.search(".*_(\\d+)_(\\d+)", name)
+			txt1=("{}-{}".format(years.group(1), years.group(2)))
+			dropBtn= DropBtn(text=txt1)
+			dropBtn.bind(on_release=lambda dropBtn: dropdown.select(dropBtn))
+			dropdown.add_widget(dropBtn)
+
+		dropdown.open(instance)
+		dropdown.bind(on_select=lambda instance, btn: self.onSelect(instance,btn, self.ids.yearBtn1))
+
+	def onSelect(self, inst, btn, mainBtn):
+		mainBtn.text= btn.text
+		print("Button text=",btn.text)
+	#-------------------------------------------------------------------#
+
+	def openUStudInfo(self):					#for Update Student information open popup
+		if(self.ids.reg_input.text==""):
+			op=InfoPopup()
+			op.ids.label.text="Please enter Reg. no. !"
+			op.open()
+		elif(self.ids.yearBtn1.text=="Select Year" or self.ids.courseBtn2.text=="Select Course"):
+			op=InfoPopup()
+			op.ids.label.text="Please Select All required Buttons !"
+			op.open()
+		else:
+			UpdateStudentInfo().open()
+
+	def openDelWarnPopup(self):
+		if(self.ids.yearBtn.text=="Select Year" or self.ids.courseBtn1.text=="Select Course"):
+			op=InfoPopup()
+			op.ids.label.text="Please Select All required Buttons !"
+			op.open()
+		else:
+			DeleteWarnPopup().open()
+
+#Main App
+
+class AccountManagementSystem(App,Database):
+	theme_cls = ThemeManager()
+	theme_cls.primary_palette = 'DeepPurple'
+
+	red_cls=ThemeManager()
+	red_cls.primary_palette = 'Red'
+
+	green_cls=ThemeManager()
+	green_cls.primary_palette = 'Green'
+
+	def on_start(self):	
+		table_list= self.findTables("student_main.db")
+		for name in table_list:
+			years= re.search(".*_(\\d+)_(\\d+)", name)
+			txt.append("{}-{}".format(years.group(1), years.group(2)))
+
+	def build(self):
+		return Builder.load_file("gui.kv")
+
+
+
+if __name__ == '__main__':
+
+	Window.maximize()
+	#Window.size=(900,700)
+	Window.minimum_width= 900
+	Window.minimum_height=700
+	print("window size:",Window.size)
+	print(Window.position)
+
+	AccountManagementSystem().run()
