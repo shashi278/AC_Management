e3d81a352d6ef747bf0db6a45896ea17949edead
shashi278 <shashiranjankv@gmail.com>
2019-09-16 15:05:43 +0530

    Few Changes nothing major

---
+++ src/generate_fee_receipt.py
@@ -0,0 +1,217 @@
+#####pip install fpdf2
+
+from fpdf import FPDF
+
+
+class PDF(FPDF):
+    def header(self):
+        # Logo
+        self.image("media/images/logo.png", 10, 8, 33)
+
+        self.set_font("Arial", "B", 14)
+        self.cell(93)
+        self.cell(
+            30, 10, "INDIAN INSTITUTE OF INFORMATION TECHNOLOGY,KALYANI", 0, 0, "C"
+        )
+
+        self.ln(8)  # used to give vertical space of box
+        self.cell(40)  # used to give horizontal space from left side
+        self.set_font("Arial", "", 11)
+
+        self.multi_cell(
+            0.0,
+            5.5,
+            "(Autonomous insitution under MHRD,Govt. of India & Department of\n\
+		Information Technology & ELectronics ,Govt. of West Bengal)\n c/o WEBEL IT Park,Opposite of Kalyani Water Treatment Plant\n\
+		Near Buddha Park Dist. Nadia P.O Kalyani PIN-741235,West Bengal\nEmail-office@iiitkalyani.ac.in, website- www.iiitkalyani.ac.in",
+            0,
+            0,
+            "C",
+        )
+
+        self.set_draw_color(0, 1, 1)
+        self.line(10, 50, 200, 50)
+        self.ln(33)
+
+    # Page footer
+    def footer(self):
+        self.set_y(-15)  # Position at 1.5 cm from bottom
+        self.set_font("Arial", "I", 8)
+        self.cell(0, 10, "Page " + str(self.page_no()) + "/{nb}", 0, 0, "C")
+
+
+def callpdf(personalinfo, feeinfo):
+    # Instantiation of inherited class
+    pdf = PDF()
+    pdf.alias_nb_pages()
+    pdf.add_page()
+
+    pdf.ln(6)
+    pdf.set_font("Times", "B", 14)
+    pdf.cell(0, 5, "Fee Details", 0, 1, "C")
+
+    pdf.ln(5)
+    pdf.set_font("Times", "B", 12)
+    pdf.multi_cell(
+        0.0,
+        7.0,
+        "Name: "
+        + personalinfo["name"]
+        + "\nReg No: "
+        + personalinfo["reg"]
+        + "                                                                                \
+	                                       Batch: "
+        + personalinfo["batch"]
+        + "\n\
+		Course & Stream: "
+        + personalinfo["course"]
+        + "("
+        + personalinfo["stream"]
+        + ")"
+        + "                                                                                         \
+		Tution Fee: "
+        + personalinfo["fee"],
+    )
+
+    pdf.ln(30)
+    pdf.set_font("Times", "B", 13)
+    pdf.cell(
+        192,
+        9,
+        "  Semester            Paid                    Due              Late Fine             Date                         Tid",
+        1,
+    )
+
+    # Data of Semesters
+    pdf.cell(-140)
+    pdf.ln(10)
+    for info in feeinfo:
+        pdf.cell(25, 14, info["sem"], 1, 0, "C")
+        pdf.cell(31, 14, info["paid"], 1, 0, "C")
+        pdf.cell(31, 14, info["due"], 1, 0, "C")
+        pdf.cell(27, 14, info["late"], 1, 0, "C")
+        pdf.cell(32, 14, info["date"], 1, 0, "C")
+        pdf.cell(46, 14, info["tid"], 1, 0, "C")
+        pdf.ln(14)
+
+    pdf.set_y(-35)
+    pdf.cell(0, 10, "Singnature of Accountant", 0, 0, "R")
+    pdf.output(personalinfo["reg"] + ".pdf", "F")
+
+
+if __name__ == "__main__":
+    fee_info = [
+        {
+            "sem": "1",
+            "paid": "94700",
+            "due": "1000",
+            "late": "1000",
+            "date": "12-12-2017",
+            "tid": "STG454355434512345",
+        },
+        {
+            "sem": "2",
+            "paid": "94500",
+            "due": "200",
+            "late": "1000",
+            "date": "12-12-2018",
+            "tid": "QAG4545",
+        },
+        {
+            "sem": "3",
+            "paid": "93700",
+            "due": "1000",
+            "late": "0",
+            "date": "12-12-2019",
+            "tid": "SAG4745",
+        },
+        {
+            "sem": "4",
+            "paid": "64700",
+            "due": "0",
+            "late": "0",
+            "date": "12-12-2020",
+            "tid": "SAG4545",
+        },
+        {
+            "sem": "5",
+            "paid": "94700",
+            "due": "1000",
+            "late": "0",
+            "date": "12-12-2021",
+            "tid": "SAG4555",
+        },
+        {
+            "sem": "6",
+            "paid": "64700",
+            "due": "1000",
+            "late": "0",
+            "date": "12-12-2021",
+            "tid": "SAJ4545",
+        },
+        {
+            "sem": "7",
+            "paid": "91730",
+            "due": "1000",
+            "late": "1000",
+            "date": "12-12-2015",
+            "tid": "SAG4545",
+        },
+        {
+            "sem": "8",
+            "paid": "14500",
+            "due": "1000",
+            "late": "1000",
+            "date": "12-12-2019",
+            "tid": "LAG4545",
+        },
+        {
+            "sem": "5",
+            "paid": "94700",
+            "due": "1000",
+            "late": "0",
+            "date": "12-12-2021",
+            "tid": "SAG4555",
+        },
+        {
+            "sem": "6",
+            "paid": "64700",
+            "due": "1000",
+            "late": "0",
+            "date": "12-12-2021",
+            "tid": "SAJ4545",
+        },
+        {
+            "sem": "7",
+            "paid": "91730",
+            "due": "1000",
+            "late": "1000",
+            "date": "12-12-2015",
+            "tid": "SAG4545",
+        },
+        {
+            "sem": "8",
+            "paid": "14500",
+            "due": "1000",
+            "late": "1000",
+            "date": "12-12-2019",
+            "tid": "LAG4545",
+        },
+        {
+            "sem": "9",
+            "paid": "67740",
+            "due": "1000",
+            "late": "1000",
+            "date": "12-12-2019",
+            "tid": "SAH4545",
+        },
+    ]
+    personal_info = {
+        "name": "Anand Kumar",
+        "reg": "213",
+        "batch": "2017-2021",
+        "course": "B.tech",
+        "stream": "cse",
+        "fee": "94700",
+    }
+    callpdf(personal_info, fee_info)
--- src/main.py
+++ src/main.py
@@ -1,66 +1,17 @@
 """
-	
 	Account Management System
 	Date: 24/02/2019
 """
 
-# imports here
+# minimal main file
+
 from kivy.app import App
 from kivy.lang import Builder
-
-"""
-from kivy.uix.screenmanager import ScreenManager, Screen, RiseInTransition
-from kivy.uix.modalview import ModalView
-from kivy.uix.popup import Popup
-from kivy.uix.button import Button
-from kivy.uix.togglebutton import ToggleButton
-from kivy.uix.textinput import TextInput
-from kivy.uix.behaviors.touchripple import TouchRippleBehavior
-from kivy.uix.dropdown import DropDown
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.label import Label
-from kivy.properties import (
-    ObjectProperty,
-    NumericProperty,
-    StringProperty,
-    ListProperty,
-)
 from kivy.core.window import Window, WindowBase
-from kivy.animation import Animation
-from kivy.clock import Clock
-from kivy.metrics import dp, sp
-from kivy.garden.filebrowser import FileBrowser
-from kivy.utils import platform
-from kivy.utils import get_color_from_hex as C
 
 from kivymd.theming import ThemeManager
-from kivymd.uix.picker import MDThemePicker
-from kivymd.uix.button import (
-    MDRaisedButton,
-    MDRectangleFlatButton,
-    MDFloatingActionButton,
-)
-from kivymd.uix.textfield import MDTextField, MDTextFieldRound, MDTextFieldRect
-from kivymd.uix.snackbar import Snackbar
-from kivymd.uix.list import OneLineListItem
-from kivymd.uix.tab import *
 
-import re
-import xlrd
-import os
-from os.path import sep, expanduser, isdir, dirname
-from random import choice
-from time import sleep
-from sqlite3 import Error
-"""
-from kivy.core.window import Window, WindowBase
 from database import Database
-from kivymd.theming import ThemeManager
-
-# from hoverable import HoverBehavior
-
 from screens import ScreenManager, HomeScreen, UserScreen, ProfilePage, AdminScreen
 
 from custom_buttons import CircularToggleButton, DropBtn
@@ -68,16 +19,12 @@ from custom_buttons import CircularToggleButton, DropBtn
 # Main App
 class AccountManagementSystem(App, Database):
     theme_cls = ThemeManager()
-    # theme_cls.primary_palette = 'Blue'
-    # theme_cls.theme_style='Light'
-    # theme_cls.accent_hue= "500"
 
     def build(self):
         return Builder.load_file("gui.kv")
 
 
 if __name__ == "__main__":
-
     Window.maximize()
     Window.minimum_width = 900
     Window.minimum_height = 700
