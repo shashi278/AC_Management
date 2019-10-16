0ffbde0c003f70881e1ca893c7fdf72dd7ee0508
shashi278 <shashiranjankv@gmail.com>
2019-10-16 19:48:24 +0530

    Added animator

---
+++ src/animator/base.py
@@ -0,0 +1,54 @@
+from kivy.clock import Clock
+from functools import partial
+import time
+
+class Animator():
+	"""
+	Animator
+	========
+	Base Class for Animators
+
+	Parameters
+	----------
+	widget: widget to be animated
+
+	duration: [optional], defaults to 1
+
+	repeat: [optional], defaults to True
+	
+	Attributes
+	----------
+	anim_complete:
+		Called internally upon completition of an animation
+
+	_initialize:
+		Used to set properties of the 'widget' prior to animation
+
+	"""
+
+	def __init__(self, widget, duration=1, repeat=True):
+		self.widget= widget
+		self.duration= duration
+		self._repeat= repeat
+		self._original= {}
+		self.attr=['opacity','height','width','pos_hint','angle']
+
+		for key in self.attr:
+			self._original[key]= getattr(self.widget, key)
+
+	@classmethod
+	def anim_complete(cls, obj, inst, widget):
+		if obj._repeat:
+			for key, val in obj._original.items():
+				setattr(widget, key, val)
+
+			inst.unbind(on_complete= obj.anim_complete)
+			time.sleep(0.3)		#just to make repeatition visually clear
+			Clock.schedule_once(partial(obj.start_, ), 0)
+
+		else:
+			inst.stop(widget)
+
+	def _initialize(self, **kwargs):
+		for key, val in kwargs.items():
+			setattr(self.widget, key, val)
---
+++ src/animator/rotateout.py
@@ -0,0 +1,86 @@
+from kivy.animation import Animation
+from functools import partial
+
+from .base import Animator
+
+__all__= ('RotateOutAnimator', 'RotateOutDownLeftAnimator', 'RotateOutDownRightAnimator', 'RotateOutUpLeftAnimator', 'RotateOutUpRightAnimator')
+
+#rotate out
+class RotateOutAnimator(Animator):
+	def start_(self, tmp=None):
+		props=["angle","opacity"]
+
+		vals=[-200,0]
+		anim= Animation(
+			d= self.duration,
+			**dict(zip(props,vals))
+			)
+
+		anim.cancel_all(self.widget)
+		anim.start(self.widget)
+		anim.bind(on_complete= partial(self.anim_complete, self))
+
+class RotateOutDownLeftAnimator(Animator):
+	def start_(self, tmp=None):
+		pivot= (self.widget.x-self.widget.width/2,self.widget.y)
+		self.widget.origin_= pivot
+
+		props=["angle","opacity"]
+		vals=[-90,0]
+		anim= Animation(
+			d= self.duration,
+			**dict(zip(props,vals))
+			)
+
+		anim.cancel_all(self.widget)
+		anim.start(self.widget)
+		anim.bind(on_complete= partial(self.anim_complete, self))
+
+
+class RotateOutDownRightAnimator(Animator):
+	def start_(self, tmp=None):
+		pivot= (self.widget.x+3*self.widget.width/2,self.widget.y)
+		self.widget.origin_= pivot
+
+		props=["angle","opacity"]
+		vals=[90,0]
+		anim= Animation(
+			d= self.duration,
+			**dict(zip(props,vals))
+			)
+		
+		anim.cancel_all(self.widget)
+		anim.start(self.widget)
+		anim.bind(on_complete= partial(self.anim_complete, self))
+
+class RotateOutUpLeftAnimator(Animator):
+	def start_(self, tmp=None):
+		pivot= (self.widget.x-self.widget.width/2,self.widget.y)
+		self.widget.origin_= pivot
+
+		props=["angle","opacity"]
+		vals=[90,0]
+		anim= Animation(
+			d= self.duration,
+			**dict(zip(props,vals))
+			)
+		
+		anim.cancel_all(self.widget)
+		anim.start(self.widget)
+		anim.bind(on_complete= partial(self.anim_complete, self))
+
+class RotateOutUpRightAnimator(Animator):
+	def start_(self, tmp=None):
+		pivot= (self.widget.x+3*self.widget.width/2,self.widget.y)
+		self.widget.origin_= pivot
+
+		props=["angle","opacity"]
+		vals=[-90,0]
+		anim= Animation(
+			d= self.duration,
+			**dict(zip(props,vals))
+			)
+		
+		anim.cancel_all(self.widget)
+		anim.start(self.widget)
+		anim.bind(on_complete= partial(self.anim_complete, self))
